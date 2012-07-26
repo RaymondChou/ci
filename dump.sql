a3e344a5157cdc2faa2bc180a7ef15eea72ca026
sirfilip <github.sirfilip@gmail.com>
2012-07-26 02:40:12 +0200

    Updated the database schema

--- dump.sql
+++ dump.sql
@@ -16,6 +16,35 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `comments`
+--
+
+DROP TABLE IF EXISTS `comments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `comments` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `post_id` int(10) unsigned NOT NULL,
+  `created_at` int(10) unsigned NOT NULL,
+  `status` tinyint(3) unsigned DEFAULT '0',
+  `content` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comments`
+--
+
+LOCK TABLES `comments` WRITE;
+/*!40000 ALTER TABLE `comments` DISABLE KEYS */;
+INSERT INTO `comments` VALUES (1,'filip@example.com','filip',2,1343262291,0,'this is great article'),(2,'filip@example.com','filp',2,1343262346,0,'this is another comment'),(3,'filip@example.com','filip',2,1343262427,0,'comment number 3'),(4,'filip@example.com','filip',2,1343262757,0,'wow this is a great article'),(5,'filip@example.com','filip',2,1343262774,0,'another great article'),(6,'filip@example.com','filip',2,1343262795,0,'perfect');
+/*!40000 ALTER TABLE `comments` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `posts`
 --
 
@@ -31,7 +60,7 @@ CREATE TABLE `posts` (
   `created_at` int(10) unsigned DEFAULT NULL,
   `published_at` int(10) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,7 +69,7 @@ CREATE TABLE `posts` (
 
 LOCK TABLES `posts` WRITE;
 /*!40000 ALTER TABLE `posts` DISABLE KEYS */;
-INSERT INTO `posts` VALUES (2,'Hello World','This is my first post.',1,2,1342361379,1342361379);
+INSERT INTO `posts` VALUES (2,'Hello World','This is my first post.',1,2,1342361379,1342361379),(3,'My Second post','This is the content of my second post',1,2,1342367765,1342367765);
 /*!40000 ALTER TABLE `posts` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -82,4 +111,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2012-07-15 16:10:05
+-- Dump completed on 2012-07-26  2:40:02
